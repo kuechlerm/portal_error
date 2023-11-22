60af15b3f534c089db59b941e458903f4ae7cfef
Martin Küchler <kuechlerm@gmail.com>
2023-11-22 10:32:16 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Portal from './Portal.svelte';
+</script>
+
+<p>Test Test Test Test Test Test Test Test Test Test</p>
+<p>Test Test Test Test Test Test Test Test Test Test</p>
+<p>Test Test Test Test Test Test Test Test Test Test</p>
+<p>Test Test Test Test Test Test Test Test Test Test</p>
+<p>Test Test Test Test Test Test Test Test Test Test</p>
+
+<Portal>
+  <div style="background: blue; padding:10px; position:fixed; top:0; ">
+    <button on:click={() => console.log('It works!')}
+      >Should cause a console message</button
+    >
+  </div>
+</Portal>
